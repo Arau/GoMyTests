0df740bea35d19a0240e0245fe0c4322990d26e3
Ferran Arau <ferran.arau.castell@gmail.com>
2019-01-06 14:47:35 +0000

    Buffering and blocking channels

--- routines/channels.go
+++ routines/channels.go
@@ -1,6 +1,16 @@
 package main
 
 import "fmt"
+import "time"
+
+func worker(done chan bool) {
+    fmt.Println("Working...")
+    time.Sleep(time.Second)
+    fmt.Println("done")
+
+    done <- true
+}
+
 
 func main() {
 
@@ -8,7 +18,26 @@ func main() {
 
     go func() { messages <- "ping" }()
 
-    msg := <-messages
+    msg := <-messages // Block until both sender and reciver are ready, so I don't have to implement any sync
     fmt.Println(msg)
+    fmt.Println()
+
+
+    // Buffered channel
+    messages = make(chan string, 2)
+    messages <- "buffered"
+    messages <- "channel"
+
+    fmt.Println(<-messages)
+    fmt.Println(<-messages)
+    
+
+    // Blocking channels
+    done := make(chan bool, 1)
+    go worker(done)
+
+    <-done // Blocking until the goroutine notifies that is ready
+    fmt.Println("After done")
 }
+
 
